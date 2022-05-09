185f2ef46fceca57e0f2e7743b0628554248e2f1
Omotola Macaulay <tolamacaulay@gmail.com>
2022-05-09 16:32:04 +0100

    Create user model and validation and hash passwords

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express()
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false}));
 
 app.get('/', (req, res) => res.send('hello woeld'))
 
