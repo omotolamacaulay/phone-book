bef98ff10d5e7f3d7e78cd6b83abdc35a7a5d097
Omotola Macaulay <tolamacaulay@gmail.com>
2022-06-16 15:54:49 +0100

    Build Frontend CRUD

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,22 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import ContactState from './context/contact/ContactState';
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />}  />
-          </Routes>
-        </div>
-      </Fragment>
-    </Router>
-   
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />}  />
+            </Routes>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 }
 
